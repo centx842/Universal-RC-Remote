a2a366b1e6e70b1091d72c2cdec8be57a172ac6c
Muhammad Sohaib bin Kashif <93905959+sohaib-kashif-97@users.noreply.github.com>
2025-09-04 14:20:55 +0500

    Color Scheme and Basic Working

--- RCApp/UI.py
+++ RCApp/UI.py
@@ -1,38 +1,122 @@
 from tkinter import *
 import customtkinter as ctk
+from customtkinter import CTkButton, CTkCanvas
+from RCApp.commands import ButtonCommands, JoystickCommands
 
 
 class AppUI:
-    def __init__(self, root, commands):
+    
+    def __init__(self, root):
+                
+        #Defining Buttons and JoySticks
         self.root = root
-        self.commands = commands
+        self.commands = ButtonCommands(root)
+        self.power = self.commands.powerStatus
+        self.joystick_commands = JoystickCommands(root)
+        
+        # Define color scheme
+        self.color_scheme = {"LG":"#B1ABAB",    #Lighter-Gray
+                        "MG": "#808080",        #Medium-Gray
+                        "DG": "#505050",        #Darker-Gray
+                        "Bl": "#202020",        #Black  
+                        "G": "#00FF00",         #Green
+                        "R": "#FF0000"}         #Red
+        
+        #Filling the background color of the main window
+        self.root.configure(bg=self.color_scheme["LG"])
+        ctk.set_appearance_mode("dark")
+
+        #Populate Buttons and JoySticks for Controllers
         self.create_widgets()
 
+
     def create_widgets(self):
-        self.label = Label(self.root, text="Welcome to the Universal RC Remote Controller", font=("Arial", 14))
-        self.label.grid(column=0, row=0, padx=10, pady=10)
 
-        self.btn1 = ctk.CTkButton(self.root, text="Power", width=10, command=self.check_power(), hover_color=("gray"))
-        self.btn1.grid(column=0, row=1, padx=5, pady=5)
+        # Main Label
+        self.label = Label(self.root, text="Universal RC Remote Controller", font=("Arial", 14))
+        self.label.place(x=275, y=25)
 
+
+        # Control Buttons
+        self.btn1 = CTkButton(self.root, text="Power", width=10 ,  hover_color= self.color_scheme["DG"], 
+                              fg_color=self.color_scheme["R"], command=self.toggle_power)         
+        self.btn1.place(x=580, y=70)
+
+        self.btn2 = CTkButton(self.root, text="Stop", width=10 , hover_color= self.color_scheme["DG"], fg_color=self.color_scheme["R"])
+        self.btn2.place(x=680, y=70)
+
+
+        # Right Directional Pad Buttons
+        self.d1 = CTkButton(self.root, text="D1", width=10, height=30 , fg_color= self.color_scheme["MG"],
+                             hover_color= self.color_scheme["DG"], border_color=self.color_scheme["Bl"], border_width=2)
+        self.d1.place(x=195, y=175)
+
+        self.d2 = CTkButton(self.root, text="D2", width=10, height=30 , fg_color= self.color_scheme["MG"],
+                             hover_color= self.color_scheme["DG"], border_color= self.color_scheme["Bl"], border_width=2)
+        self.d2.place(x=190, y=275)
+
+        self.d3 = CTkButton(self.root, text="D3", width=10, height=30 , fg_color= self.color_scheme["MG"],
+                             hover_color= self.color_scheme["DG"], border_color= self.color_scheme["Bl"], border_width=2)
+        self.d3.place(x=130, y=225)
+
+        self.d4 = CTkButton(self.root, text="D4", width=10, height=30 , fg_color= self.color_scheme["MG"],
+                             hover_color= self.color_scheme["DG"], border_color= self.color_scheme["Bl"], border_width=2)
+        self.d4.place(x=250, y=225)
         
-        # btn2 = Button(self.root, text="Forward", width=10)
-        # btn2.grid(column=1, row=1, padx=5, pady=5)
 
-        # bt3n3 = Button(self.root, text="Backward", width=10)
-        # bt3n3.grid(column=2, row=1, padx=5, pady=5)
+        # Left Joystick
+        self.js1 = CTkCanvas(self.root, width=120, height=120, bg="gray", highlightthickness=0)
+        self.js1.place(x=250, y=375)
+        # self.js1.create_oval(20, 20, 100, 100, fill="gray")
+        self.js1.create_oval(20, 20, 100, 100, fill="white")
+        
 
-        # btn4 = Button(self.root, text="Left", width=10)
-        # btn4.grid(column=0, row=2, padx=5, pady=5)
+        # Right Command Pad Buttons
+        self.btn3 = CTkButton(self.root, text="Forward", width=10, fg_color= self.color_scheme["MG"],
+                               hover_color= self.color_scheme["DG"], border_color= self.color_scheme["Bl"], border_width=2)
+        self.btn3.place(x=595, y=175)
 
-        # btn5 = Button(self.root, text="Right", width=10)
-        # btn5.grid(column=1, row=2, padx=5, pady=5)
+        self.btn4 = CTkButton(self.root, text="Backward", width=10, fg_color= self.color_scheme["MG"],
+                               hover_color= self.color_scheme["DG"], border_color= self.color_scheme["Bl"], border_width=2)
+        self.btn4.place(x=590, y=275)
 
-        # btn6 = Button(self.root, text="Stop", width=10)
-        # btn6.grid(column=2, row=2, padx=5, pady=5)
+        self.btn5 = CTkButton(self.root, text="Left", width=10, fg_color= self.color_scheme["MG"],
+                               hover_color= self.color_scheme["DG"], border_color= self.color_scheme["Bl"], border_width=2)
+        self.btn5.place(x=530, y=225)
 
-    def check_power(self):
-        if self.commands.powerStatus == True:
-            self.btn1.configure(fg_color = "Green")
-        elif self.commands.powerStatus == False:
-            self.btn1.configure(fg_color = "Red")
+        self.btn6 = CTkButton(self.root, text="Right", width=10, fg_color= self.color_scheme["MG"],
+                               hover_color= self.color_scheme["DG"], border_color= self.color_scheme["Bl"], border_width=2)
+        self.btn6.place(x=680, y=225)
+
+
+        # Right Joystick
+        self.js2 = CTkCanvas(self.root, width=120, height=120, bg="gray", highlightthickness=0)
+        self.js2.place(x=450, y=375)
+        # self.js2.create_oval(20, 20, 100, 100, fill="gray")
+        self.js2.create_oval(20, 20, 100, 100, fill="white")
+
+
+        # Additional Buttons
+        self.aux1 = CTkButton(self.root, text="AUX1", width=40, height=10 , fg_color= self.color_scheme["MG"],
+                             hover_color= self.color_scheme["DG"], border_color=self.color_scheme["Bl"], border_width=2)
+        self.aux1.place(x=160, y=130)
+
+        self.aux2 = CTkButton(self.root, text="AUX2", width=40, height=10 , fg_color= self.color_scheme["MG"],
+                             hover_color= self.color_scheme["DG"], border_color= self.color_scheme["Bl"], border_width=2)
+        self.aux2.place(x=320, y=130)
+
+        self.aux3 = CTkButton(self.root, text="AUX3", width=40, height=10 , fg_color= self.color_scheme["MG"],
+                             hover_color= self.color_scheme["DG"], border_color= self.color_scheme["Bl"], border_width=2)
+        self.aux3.place(x=480, y=130)
+
+        self.aux4 = CTkButton(self.root, text="AUX4", width=40, height=10 , fg_color= self.color_scheme["MG"],
+                             hover_color= self.color_scheme["DG"], border_color= self.color_scheme["Bl"], border_width=2)
+        self.aux4.place(x=640, y=130)
+
+
+    def toggle_power(self):
+        self.power = self.commands.toggle_power()
+        if self.power == True:
+            self.btn1.configure(fg_color = self.color_scheme["G"])
+        elif self.power == False:
+            self.btn1.configure(fg_color = self.color_scheme["R"])
--- RCApp/commands.py
+++ RCApp/commands.py
@@ -1,7 +1,10 @@
 from tkinter import *
 import customtkinter as ctk
 
+
+
 class ButtonCommands:
+
     def __init__(self, root):
         self.root = root
         self._powerStatus = False      
@@ -11,13 +14,20 @@ class ButtonCommands:
     def powerStatus(self):
         return self._powerStatus
     
-    def togglePower(self):
-        if self._powerStatus == False:
-            self._powerStatus = True
-            print("POWER ON!")
-        else:
-            self._powerStatus = False
-            print("POWER OFF!")
+    #Set Method for the 'PowerStatus' attribute. 
+    @powerStatus.setter
+    def powerStatus(self, status):
+        self._powerStatus = status
+    
+    def toggle_power(self):
+        if self.powerStatus == True:
+            print("Power Off")
+            self.powerStatus = False
+        elif self.powerStatus == False:
+            print("Power On")
+            self.powerStatus = True
+        return self.powerStatus
+
 
     def forward(self):
         if self._powerStatus == True:
@@ -46,3 +56,20 @@ class ButtonCommands:
     def stop(self):
         if self._powerStatus == True:
             print("Stopping")
+
+class JoystickCommands:
+
+    def __init__(self, root):
+        self.root = root
+
+    def move_up(self):
+        print("Joystick moved up")
+
+    def move_down(self):
+        print("Joystick moved down")
+
+    def move_left(self):
+        print("Joystick moved left")
+
+    def move_right(self):
+        print("Joystick moved right")
