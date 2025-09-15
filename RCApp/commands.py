47d3229523c6d8388030c583c3902fc24631070a
Muhammad Sohaib bin Kashif <93905959+sohaib-kashif-97@users.noreply.github.com>
2025-09-15 10:02:16 +0500

    More Interactions Added
    - JoySticks Movement #4
    - Slider Options change
    - Button Interact only when Remote is ON and the Comms Type is selected.

--- RCApp/commands.py
+++ RCApp/commands.py
@@ -1,13 +1,14 @@
 from tkinter import *
-import customtkinter as ctk
-
+# import customtkinter as ctk
+from RCApp.utils import MQTTClient
 
 
 class ButtonCommands:
 
     def __init__(self, root):
         self.root = root
-        self._powerStatus = False      
+        self._powerStatus = False 
+        self.mqtt_client = MQTTClient()     
 
     #Get Method for the 'PowerStatus' attribute. 
     @property
@@ -28,48 +29,64 @@ class ButtonCommands:
             self.powerStatus = True
         return self.powerStatus
 
+    def send_command(self, text):
+        if self._powerStatus == True:
+            # self.mqtt_client.send_command(text)
+            pass
 
-    def forward(self):
-        if self._powerStatus == True:
-            print("Moving Forward")
-
-    def backward(self):
-        if self._powerStatus == True:
-            print("Moving Backward")
-
-    def accelerate(self):
-        if self._powerStatus == True:
-            print("Increasing Speed!")
-
-    def decelerate(self):
-        if self._powerStatus == True:
-            print("Decreasing Speed!")
-    
-    def left(self):
-        if self._powerStatus == True:
-            print("Turning Left")
-
-    def right(self):
-        if self._powerStatus == True:
-            print("Turning Right")
-
-    def stop(self):
-        if self._powerStatus == True:
-            print("Stopping")
 
 class JoystickCommands:
 
     def __init__(self, root):
         self.root = root
+        self.js1 = None
+        self.js1_knob = None
+        self.js2 = None
+        self.js2_knob = None
+        self.js1_dragging = False
+        self.js2_dragging = False
 
-    def move_up(self):
-        print("Joystick moved up")
+    def set_joysticks(self, js1, js1_knob, js2, js2_knob):
+        self.js1 = js1
+        self.js1_knob = js1_knob
+        self.js2 = js2
+        self.js2_knob = js2_knob
 
-    def move_down(self):
-        print("Joystick moved down")
+    # Left Joystick bindings
+    def start_drag_js1(self, event):
+        self.js1_dragging = True
 
-    def move_left(self):
-        print("Joystick moved left")
+    def drag_js1(self, event):
+        if self.js1_dragging:
+            # Limit knob movement within joystick bounds
+            x = min(max(event.x, 30), 90)
+            y = min(max(event.y, 30), 90)
+            self.js1.coords(self.js1_knob, x-10, y-10, x+10, y+10)
 
-    def move_right(self):
-        print("Joystick moved right")
+    def end_drag_js1(self, event):
+        self.js1_dragging = False
+
+    # Right Joystick bindings
+    def start_drag_js2(self, event):
+        self.js2_dragging = True
+
+    def drag_js2(self, event):
+        if self.js2_dragging:
+            x = min(max(event.x, 30), 90)
+            y = min(max(event.y, 30), 90)
+            self.js2.coords(self.js2_knob, x-10, y-10, x+10, y+10)
+
+    def end_drag_js2(self, event):
+        self.js2_dragging = False
+
+
+class CameraCommands:
+
+    def __init__(self, root):
+        self.root = root
+
+    def start_camera(self):
+        print("Camera Started")
+
+    def stop_camera(self):
+        print("Camera Stopped")
